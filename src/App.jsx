b506fbc0fb446dac92275e26f42cb3e151c8cb6e
Devang Dhaka <devangdhaka.ai@gmail.com>
2026-06-12 19:39:47 +0530

    setup project, build Counter and LightSwitch components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from './components/Counter'
+// import Counter from './components/LightSwitch'
+
+function App() {
+  return (
+    <div>
+      <h1>TaskFlow</h1>
+      <Counter />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/LightSwitch.jsx
@@ -0,0 +1,14 @@
+// import { useState } from "react"
+
+// function LightSwitch() {
+//   const [isOn, setIsOn] = useState(false)
+
+//   return (
+//     <div>
+//       <p>Light is {isOn ? 'ON' : 'OFF'}</p>
+//       <button onClick={() => setIsOn(!isOn)}>Toggle</button>
+//     </div>
+//   )
+// }
+
+// export default LightSwitch
