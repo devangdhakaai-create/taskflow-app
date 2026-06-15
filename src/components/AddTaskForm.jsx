0b81af9ef307aa8add4b65a9ab29ebd0d37fc85b
Devang Dhaka <devangdhaka.ai@gmail.com>
2026-06-15 10:33:59 +0530

    add AddTaskForm component with controlled input and task creation

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function AddTaskForm({ onAdd }) {
+    const [title, setTitle] = useState('')
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (title.trim() === '') return
+
+        onAdd(title)
+        setTitle('')
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type = "text"
+                value = {title}
+                onChange = {(e) => setTitle(e.target.value)}
+                placeholder = "Enter task title"
+            />
+            <button type = "submit">Add Task</button>
+        </form>
+    )
+    
+
+export default AddTaskForm
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react"
-import TaskCard from "./TaskCard"
+import { useState } from 'react'
+import TaskCard from './TaskCard'
+import AddTaskForm from './AddTaskForm'
 
 function TaskList() {
   const [tasks, setTasks] = useState([
@@ -7,29 +8,43 @@ function TaskList() {
     { id: 2, title: "Walk the dog", done: true },
     { id: 3, title: "Finish TaskFlow", done: false }
   ])
+
   const removeTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
-    const toggleTask = (id) => {
-        setTasks(
-            tasks.map((task) =>
-            task.id === id? {...task, done: !task.done}:task)
-        )
-    }
   }
+
+  const toggleTask = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, done: !task.done } : task
+      )
+    )
+  }
+
+  const addTask = (title) => {
+    const newTask = {
+      id: Date.now(),
+      title: title,
+      done: false
+    }
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div>
-        {tasks.map((task) => (
-            <TaskCard
-                key={task.id}
-                id={task.id}
-                title={task.title}
-                done={task.done}
-                onRemove={removeTask}
-                onToggle={toggleTask}
-            />
-        ))
-        }
+      <AddTaskForm onAdd={addTask} />
+      {tasks.map((task) => (
+        <TaskCard
+          key={task.id}
+          id={task.id}
+          title={task.title}
+          done={task.done}
+          onRemove={removeTask}
+          onToggle={toggleTask}
+        />
+      ))}
     </div>
   )
 }
+
 export default TaskList
